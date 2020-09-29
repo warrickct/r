c2c3eb820a1675b083f462307814a28b721af7de
warrickct <warrickct@gmail.com>
2020-09-29 21:24:58 +1300

    contact details scss done.

--- src/js/components/contact/contact.component.tsx
+++ src/js/components/contact/contact.component.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import './contact.component.css'
+import styles from './contact.module.scss';
 
 export function Contact() {
 
     return (
-        <div className="contact">
-            <div id="mobile-detail" className="contact__detail">0274376313</div>
-            <div id="email-detail" className="contact__detail">
+        <div className={styles.contact}>
+            <div id="mobile-detail" className={styles.contact_detail}>0274376313</div>
+            <div id="email-detail" className={styles.contact_detail}>
                 warrickct@gmail.com
         </div>
-            <div id="location-detail" className="contact__detail">
+            <div id="location-detail" className={styles.contact_detail}>
                 Auckland, New Zealand
         </div>
-            <div id="github-detail" className="contact__detail">
+            <div id="github-detail" className={styles.contact_detail}>
                 github.com/warrickct
         </div>
         </div>
