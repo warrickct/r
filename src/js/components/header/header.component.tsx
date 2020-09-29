818b308c5cf36ac94a1e09fc888fb2421c7a2a14
warrickct <warrickct@gmail.com>
2020-09-29 21:16:00 +1300

    Partially fixed header. Need to fix blinking cursor.

--- src/js/components/header/header.component.tsx
+++ src/js/components/header/header.component.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import './header.component.css'
+import styles from './header.module.scss';
 
 export function Header() {
-
     return (
-        <div className="section section--no-padding section--centered">
-            <div>
-                <h1 className="banner__title" id="name">Warrick Corfe-Tan<span className="blinking-cursor">|</span></h1>
-            </div>
+        <div className={styles.banner}>
+                <h1 className={styles.banner__title} id="name">Warrick Corfe-Tan<span className={styles.blinkingCursor}>|</span></h1>
         </div>
     )
 }
