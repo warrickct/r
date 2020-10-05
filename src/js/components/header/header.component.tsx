10876e987ed46a2e9892a66949ffaf1b56241b2c
warrickct <warrickct@gmail.com>
2020-10-05 21:38:03 +1300

    Fixed blinking cursor

--- src/js/components/header/header.component.tsx
+++ src/js/components/header/header.component.tsx
@@ -4,7 +4,7 @@ import styles from './header.module.scss';
 export function Header() {
     return (
         <div className={styles.banner}>
-                <h1 className={styles.banner__title} id="name">Warrick Corfe-Tan<span className={styles.blinkingCursor}>|</span></h1>
+                <h1 className={styles.banner__title} id="name">Warrick Corfe-Tan<span className={styles.blink}>|</span></h1>
         </div>
     )
 }
