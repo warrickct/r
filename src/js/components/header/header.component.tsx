b6371f700f2edc9868d2ab3284f407a47e94492a
warrickct <warrickct@gmail.com>
2020-10-12 22:01:16 +1300

    update build. Prevent surname from breaking at hyphen.

--- src/js/components/header/header.component.tsx
+++ src/js/components/header/header.component.tsx
@@ -4,7 +4,7 @@ import styles from './header.module.scss';
 export function Header() {
     return (
         <div className={styles.banner}>
-                <h1 className={styles.banner__title} id="name">Warrick Corfe-Tan<span className={styles.blink}>|</span></h1>
+                <h1 className={styles.banner__title} id="name">Warrick <span style={{"whiteSpace": "nowrap"}}>Corfe-Tan</span><span className={styles.blink}>|</span></h1>
         </div>
     )
 }
