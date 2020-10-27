d767f9ea666d60295ecd541a0d4bb65e0c9c7450
warrickct <warrickct@gmail.com>
2020-10-27 22:00:44 +1300

    WIP print theming.

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -6,7 +6,6 @@ import { Experience } from "./experience/experience.component.tsx";
 import { Projects } from "./projects/projects.component.tsx";
 import { Education } from "./education/education.component.tsx";
 import { Miscellaneous } from "./miscellaneous/miscellaneous.component.tsx";
-// import './../../custom.css';
 import styles from './app.module.scss';
 
 
@@ -30,10 +29,15 @@ class App extends Component {
     });
   }
 
+  switchTheme() {
+    // TODO: Alter the theme to pass into child components.
+    // Pass either web version or print version.
+  }
+
   render() {
     return (
       <div className={styles.app}>
-        <Header />
+        <Header theme={styles.printTheme} />
         <Contact />
         <Experience />
         <Projects />
--- src/js/components/header/header.component.tsx
+++ src/js/components/header/header.component.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './header.module.scss';
 
-export function Header() {
+export function Header(props: any) {
+    const { theme} = props;
+
     return (
-        <div className={styles.banner}>
+        <div className={`${styles.banner} ${theme}`}>
                 <h1 className={styles.banner__title} id="name">Warrick <span style={{"whiteSpace": "nowrap"}}>Corfe-Tan</span><span className={styles.blink}>|</span></h1>
         </div>
     )
